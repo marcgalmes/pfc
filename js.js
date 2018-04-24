d95c679f5d51d21949fd7088d209b64934e0e917
mgalmes <marcgalmes2a@gmail.com>
2018-04-24 17:24:58 +0200

    mapa v2

--- js.js
+++ js.js
@@ -63,7 +63,7 @@ $(function(){
 });
 
 var loadSection = function(hash) {
-	var match = hash.match(/seccion:(\w+)/);
+	var match = hash.match(/seccion=(\w+)/);
 	if (match){
 		//oculta/muestra la seccion
 		$(".seccion").toggleClass("hide",true);
@@ -92,7 +92,9 @@ Para conectar el mapa a google maps
 
 */
 
-
+$(function(){//intenta ubicar al cargar la pagina
+	getMyLocation();
+});
 var map;
   function initMap() {
 	  
@@ -103,12 +105,9 @@ var map;
 	  zoom: 18
 	});
 	
-	poly = new google.maps.Polyline({
-	  strokeColor: '#000000',
-	  strokeOpacity: 1.0,
-	  strokeWeight: 3
-	});
-	poly.setMap(map);
+	if (window.myLocation) {
+		showMyLocation(window.myLocation);
+	}
   });
   }
   
@@ -116,63 +115,55 @@ var map;
 	// Try HTML5 geolocation.
 	if (navigator.geolocation) {
 	  navigator.geolocation.getCurrentPosition(function(position) {
-		var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+		  window.myLocation = position;
+		  showMyLocation(window.myLocation);
+	  }, function() {
+		  console.log("Error location");
+	  });
+	} else {
+		alert("Location not supported");
+	}
+  }
+  
+  function showMyLocation(position) {
 		var pos = {
 		  lat: position.coords.latitude,
 		  lng: position.coords.longitude
 		};
-		var marker1 = new google.maps.Marker({
+		var myloc = new google.maps.Marker({
+			clickable: false,
+			icon: new google.maps.MarkerImage('miUbicacion.png'),
+			// shadow: null,
+			// zIndex: 999,
 			position: pos,
-			map: map,
-			draggable: true,
-			icon: image,
-			animation: google.maps.Animation.DROP,
-			title: 'Tu ubicación'   
+			map: map
 		});
 		
-		marker1.addListener('click', function() {
+		myloc.addListener('click', function() {
 			map.setZoom(18);
-			map.setCenter(marker1.getPosition());
+			map.setCenter(myloc.getPosition());
 		});
-		//info
-		var infoWindow = new google.maps.InfoWindow({map: map});
-		infoWindow.setPosition(pos);
-		infoWindow.setContent('Location found.');
 		
 		map.setCenter(pos);
 		map.setZoom(18);
 		map.addListener('click', addLatLng);
-	  }, function() {
-		handleLocationError(true, infoWindow, map.getCenter());
-	  });
-	} else {
-	  // Browser doesn't support Geolocation
-	  handleLocationError(false, infoWindow, map.getCenter());
-	}
-  }
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-	infoWindow.setPosition(pos);
-	infoWindow.setContent(browserHasGeolocation ?
-						  'Error: The Geolocation service failed.' :
-						  'Error: Your browser doesn\'t support geolocation.');
   }
 
 function addLatLng(event) {
-	var path = poly.getPath();
-
-	// Because path is an MVCArray, we can simply append a new coordinate
-	// and it will automatically appear.
-	path.push(event.latLng);
-
 	// Add a new marker at the new plotted point on the polyline.
-	var marker = new google.maps.Marker({
+	if (typeof marker != "undefined") {
+		marker.setMap(null);//borrar el marker
+	};
+	marker = new google.maps.Marker({
 	  position: event.latLng,
-	  title: '#' + path.getLength(),
+	  title: 'Incidencia aqui',
 	  map: map
 	});
+	
+	$("#latitud").val(event.latLng.lat);
+	$("#longitud").val(event.latLng.lng);
 }
 
 function nuevaIncidencia() {
-	window.location = "#seccion:modificarIncidencia";
+	window.location = "#seccion=modificarIncidencia";
 }
